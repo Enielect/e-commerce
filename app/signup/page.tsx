77bbb2c85103239c135451c4e381e7f7c87bafe0
eniola <eniolafaithful57@gmail.com>
2024-08-19 14:06:59 +0100

    Feat: integrate the login and signup components to their pages

---
+++ app/signup/page.tsx
@@ -0,0 +1,12 @@
+import SignUp from '@/components/account/SignUp'
+import React from 'react'
+
+const SignUppage = () => {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  )
+}
+
+export default SignUppage
